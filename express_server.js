69a51181c69664552b9b4b699a71f12e82e37bf5
Ray Yu <kaito-akira@hotmail.com>
2022-10-03 19:40:42 -0700

    pre URL shortening p1 update

--- express_server.js
+++ express_server.js
@@ -9,15 +9,17 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-app.get("urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
-  res.render("urls_index", templateVars);
-});
 
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase };
   res.render("urls_show", templateVars);
 });
+
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
 
 app.get("/", (req, res) => {
   res.send("Hello! Changes");
